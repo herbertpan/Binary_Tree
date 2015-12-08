437d1043b3e87de6c1680678a1ca72f60c4d39a8
herbertpan <herbertpan@users.noreply.github.com>
2015-12-08 00:42:07 -0500

    Update myshell.cpp

--- myshell.cpp
+++ myshell.cpp
@@ -10,45 +10,50 @@
 #include <errno.h>
 using namespace std;
 
-int cnt_vertline = 0; 
+int pipe_num = 0; 
 int ifredirect = 0;
 void vect2charr(vector<string>* cmd_vct, char ** &argv);
 void close_pipes(int pipe_fd[][2]);
+//void delete_process(char*** const & argv_arr, char*** const & redi_arr, vector<string>*& redirect_arr,vector<string>& cmd_vct);
 void test_output(vector<string> *redirect_arr, char*** argv_arr, char*** redi_arr);
 int fork_loop(int *pid, int pipe_fd[][2], char*** argv_arr, char*** argk_arr);
 int redirect_process(char ** argk);
 void redirect_parse(vector<string>* vec_arr, vector<string> * redirect, char**& redi_arr);
 int redirect_judgement(char** argk, int cnt);
 
+
 void delete_process(char*** const & argv_arr, char*** const & redi_arr, vector<string>*& redirect_arr,vector<string>& cmd_vct) {
-  for(int i = 0; i <= cnt_vertline; i++){
+  for(int i = 0; i <= pipe_num; i++){
     /* to determine the size of argv_arr[i] */
     int k = 0;
+    //while(!strcmp(argv_arr[i][k], "NULL")){  k++; }
     while(argv_arr[i][k] != NULL){
       k++;
     }
+    //cout << "k in delete is nums of argv_arr[i][x] = " << k << endl;
     /************Let's Delete It***********/
     for(int j = 0; j <= k; j++) {
       delete [] argv_arr[i][j];
     }
   }
-  for(int i = 0; i <= cnt_vertline; i++){
+  for(int i = 0; i <= pipe_num; i++){
     delete [] argv_arr[i];
   }
   //delete redi_arr
   if(ifredirect != 0) {
-    for(int i = 0; i < cnt_vertline + 1; i++){
+    for(int i = 0; i < pipe_num + 1; i++){
       /* to determine the size of redi_arr[i] */
       int k = 0;
       while(redi_arr[i][k] != NULL){
 	k++;
       }
+      //cout << "k in delete is nums of redi_arr[i][x] = " << k << endl;
       /************Let's Delete It***********/
       for(int j = 0; j <= k; j++) {
 	delete [] redi_arr[i][j];
       }
     }
-    for(int i = 0; i < cnt_vertline + 1; i++){
+    for(int i = 0; i < pipe_num + 1; i++){
       delete [] redi_arr[i];
     }
   }  
@@ -88,20 +93,20 @@ void command_parse(string str, vector<string> * cmd_vct) {
   }
   for(vector<string>::iterator it = cmd_vct->begin(); it != cmd_vct->end(); ++it){
     if(*it == "|"){
-      cnt_vertline++;
+      pipe_num++;
       //cout << "I find a \"|\" " << endl;
     }
   }
 } 
 
 void adv_parse(vector<string>* cmd_vct, vector<string>* & redirect_arr, char *** argv_arr, char*** redi_arr) {
-  //int cnt_vertline = 0;   
+  //int pipe_num = 0;   
   //int ifredirect = 0; became a global variable
-  vector<string>::iterator it_pos;           // an iterator used as container
-  vector<vector<string>::iterator> it_vec;   // contains all '|' iterator
+  vector<string>::iterator it_pos;  // an iterator used as container
+  vector<vector<string>::iterator> it_vec;  //contains all '|' iterator
   for(vector<string>::iterator it = cmd_vct->begin(); it != cmd_vct->end(); ++it){
     if(*it == "|"){
-      //cnt_vertline++;
+      //pipe_num++;
       it_vec.push_back(it);
       //cout << "I find a \"|\" " << endl;
     }
@@ -110,9 +115,9 @@ void adv_parse(vector<string>* cmd_vct, vector<string>* & redirect_arr, char ***
     }
   }
   /***************************VEC_ARR****************************/
-  vector<string>* vec_arr = new vector<string>[cnt_vertline+1]();
+  vector<string>* vec_arr = new vector<string>[pipe_num+1]();
   /***************************VEC_ARR****************************/
-  for(int i = cnt_vertline; i > 0; i--){
+  for(int i = pipe_num; i > 0; i--){
     it_pos = it_vec.back();
     it_pos++;                  // *it_pos = '|'
     for(;it_pos != cmd_vct->end(); ++it_pos){
@@ -140,7 +145,7 @@ void adv_parse(vector<string>* cmd_vct, vector<string>* & redirect_arr, char ***
   vect2charr(&vec_arr[0], argv_arr[0]);
 
   /* Print Out every vec_arr[]*//*  
-  for(int i = 0; i <= cnt_vertline; i++){
+  for(int i = 0; i <= pipe_num; i++){
     while(vec_arr[i].empty() != true){
       cout << "vec_arr[" << i <<"] = " << vec_arr[i].back() << endl;
       vec_arr[i].pop_back();
@@ -155,6 +160,9 @@ void redirect_parse(vector<string>* vec_arr, vector<string> * redirect, char**& 
   for(vector<string>::iterator it = vec_arr->begin(); it < vec_arr->end(); ){
   //while(vector<string>::iterator)
   //redirect analyze
+    //cout << "*it = " << *it << endl;
+    //cout << "redirect.size = " << redirect->size() << endl;
+    //cout << redirect[0] << " " << redirect[1] << " " << redirect[2] << endl;
     if( (*it == "<") || (*it == ">") || (*it == "2>") ) {
       //cout << "*it = " << *it << endl;
       string a = *it;
@@ -162,18 +170,23 @@ void redirect_parse(vector<string>* vec_arr, vector<string> * redirect, char**& 
       redirect->push_back(a);
       redirect->push_back(b);
       it = vec_arr->erase(it);
-      it = vec_arr->erase(it);  // dont need to ++it, because vector.erase() move it forward
+      it = vec_arr->erase(it);
       /*
 	for(vector<string>::iterator it = vec_arr->begin(); it != vec_arr->end(); ++it){
 	cout << *it << endl;
 	}*/
-      vec_arr->shrink_to_fit();
+      //vec_arr->shrink_to_fit();
     }
     else{
       ++it;
     }
   }
   vect2charr(redirect, redi_arr);
+  /* cat -p < in -f > out -l, could have two < > in one command line
+  if( redirect->size() > 2) {
+    cout << "redirect.size = " << redirect->size() << endl;
+    cout << "Error: input 2 redirect symbol sequentially" << endl;
+    }*/
   /*
     for(unsigned i = 0; i < redirect->size(); i++){
     cout << "redirect[" << i << "] = " << (*redirect)[i] << endl;
@@ -243,15 +256,16 @@ int main() {
   string str;
   vector<string> direct;
   vector<string> cmd_vct;
-  //vector<string> vec_arr[cnt_vertline+1];
+  //vector<string> vec_arr[pipe_num+1];
   int status;
   char buf[100];
   int flag = 1;
   while(flag != 0){
     //cout << "flag = "<< flag << endl;
-    cnt_vertline = 0;
+    pipe_num = 0;
     ifredirect = 0;
     getcwd(buf, sizeof(buf));
+    cout.flush();
     cout << "myShell :";
     cout << buf <<" $ " ;
     /* Get the input Comnmand Line */
@@ -263,18 +277,18 @@ int main() {
     /* Parse, Split and Stored into vector<string> cmd_vct */
     command_parse(str, &cmd_vct);   
     /* Transfer string into Char*[] */
-    //cout << "cnt_vertline = " << cnt_vertline << endl;
-    pid_t pid[cnt_vertline+1];
-    int pipe_fd[cnt_vertline][2];
+    //cout << "pipe_num = " << pipe_num << endl;
+    pid_t pid[pipe_num+1];
+    int pipe_fd[pipe_num][2];
     /* Create all Pipes*/
-    for(int i = 0; i < cnt_vertline; i++){
+    for(int i = 0; i < pipe_num; i++){
       if(pipe(pipe_fd[i]) < 0) {
 	perror("Create pipe failed");
       }
     }
-    char **argv_arr[cnt_vertline+1];
-    char **redi_arr[cnt_vertline+1];
-    vector<string> * redirect_arr = new vector<string>[cnt_vertline+1]();
+    char **argv_arr[pipe_num+1];
+    char **redi_arr[pipe_num+1];
+    vector<string> * redirect_arr = new vector<string>[pipe_num+1]();
     adv_parse(&cmd_vct, redirect_arr, argv_arr, redi_arr);
     /* Test of output */
     //test_output(redirect_arr, argv_arr, redi_arr);
@@ -285,7 +299,10 @@ int main() {
       delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
       return EXIT_SUCCESS;
     }
-    if(flag == 2) { continue; }  // for input: ENTER 
+    if(flag == 2) { // enter  
+      delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
+      continue; 
+    }  
     int ifbuiltin = builtin_command(buf, &direct, argv_arr[0]);
     if(ifbuiltin == 1){
       delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
@@ -302,31 +319,23 @@ int main() {
     /* In Root Child Process */
     else if(ppid == 0) {
       /* Execute the Child Process */
-      //execute only when err occur (execve return)
       int ret = fork_loop(pid, pipe_fd, argv_arr, redi_arr);
       if(ret == -1 ) {
 	//cout << "Error occured in fork loop" << endl;
 	delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
 	return -1;
       }
+      if(pipe_num != 0){
+	close_pipes(pipe_fd);
+      }
       //after the fork loop, because no exec function, you have to delete it 
-      delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
-      waitpid(-1, NULL, 0);
-      //sleep(1);
-      return 0;
-    }
-    
-    
-    /* In Parent Process */
-    do{
-      //sleep(1);
-      close_pipes(pipe_fd);
-      wpid = waitpid(ppid, &status, 0);
+      for(int i = 0; i < pipe_num+1; i++){
+	wpid = waitpid(pid[i], &status, 0);
+      }
       if(wpid == -1) {
 	perror("error in waitpid");
 	exit(EXIT_FAILURE);
-      }
-      
+      }      
       if(WIFEXITED(status)) {
 	printf("Program exited with status %d\n", WEXITSTATUS(status));
       }
@@ -339,22 +348,36 @@ int main() {
       else if (WIFCONTINUED(status)) {
 	printf("continued\n");
       }
-      
+      close_pipes(pipe_fd);
+      //after the fork loop, because no exec function, you have to delete it 
+      //for(int i = 0; i < pipe_num+1; i++){
+	//std::cout << "Waiting for " << pid[i] << "\n";
+	//waitpid(pid[i], NULL, 0);
+      //}
+      delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);    
+      //waitpid(pid[pipe_num], NULL, 0);
+      //sleep(1);
+      return 0;
+    }
+    /* In Parent Process */
+    do{
+      close_pipes(pipe_fd);
+      wpid = waitpid(ppid, &status, 0);
     } while(!WIFEXITED(status) && !WIFSIGNALED(status));
-    
     //printf("WEXITSTATUS: %d\n", WEXITSTATUS(status));
     /* REMEMBER TO DELETE ARGV[]*/
     delete_process(argv_arr, redi_arr, redirect_arr, cmd_vct);
   } 
-  
 }
 
 int fork_loop(int *pid, int pipe_fd[][2], char*** argv_arr, char*** argk_arr) {
   //int res_stdin = dup(STDIN_FILENO);
   //int res_stdout = dup(STDOUT_FILENO);
-  for(int i = 0; i < cnt_vertline + 1; i++) {
+  for(int i = 0; i < pipe_num + 1; i++) {
     if((pid[i] = fork()) < 0) { perror("fork failed"); }
+    /**********Child Process************/
     if(pid[i] == 0) {
+      //std::cout << "pid " << getpid() << " will run " << argv_arr[i][0]<< "\n";
       //for the first command part
       if(i == 0){
 	//cout << "i = 0" << endl;
@@ -362,18 +385,18 @@ int fork_loop(int *pid, int pipe_fd[][2], char*** argv_arr, char*** argk_arr) {
 	  int ret = redirect_process(argk_arr[i]);
 	  if(ret == -1) return -1;
 	}
-	if(cnt_vertline != 0){  // pipes exist
+	if(pipe_num != 0){  // pipes exist
 	  dup2(pipe_fd[0][1], STDOUT_FILENO);
 	}
       }
       //for the last command part
-      else if(i == cnt_vertline){
-	//cout << "i = cnt_vertline" << endl;
+      else if(i == pipe_num){
+	//cout << "i = pipe_num" << endl;
 	if(ifredirect != 0){
 	  int ret = redirect_process(argk_arr[i]);
 	  if(ret == -1) return -1;
 	}
-	if(cnt_vertline != 0){  // pipes exist
+	if(pipe_num != 0){  // pipes exist
 	  dup2(pipe_fd[i-1][0], STDIN_FILENO);
 	}
       }
@@ -381,13 +404,13 @@ int fork_loop(int *pid, int pipe_fd[][2], char*** argv_arr, char*** argk_arr) {
       else{
 	//cout << "i = others" << endl;
 	/* the commands in the middle, should not have redirect*/
-	if(cnt_vertline != 0){  // pipes exist
+	if(pipe_num != 0){  // pipes exist
 	  dup2(pipe_fd[i-1][0], STDIN_FILENO);
 	  dup2(pipe_fd[i][1], STDOUT_FILENO);
 	}
       }
       //cout<<"Fork " << i <<" child process: ppid = "<< getppid() <<" pid =" <<getpid() << endl;
-      if(cnt_vertline != 0){
+      if(pipe_num != 0){
 	close_pipes(pipe_fd);
       }
       int ret = execvp(argv_arr[i][0], argv_arr[i]);
@@ -396,16 +419,16 @@ int fork_loop(int *pid, int pipe_fd[][2], char*** argv_arr, char*** argk_arr) {
 	return -1;
       }
     }
-    // sleep(1);
-    waitpid(pid[i], NULL, 0);
-    //fflush(stdout);
+    /**********Parent Process************/
+    cout.flush();
+    //waitpid(pid[i], NULL, 0);
   }
   return 0;
 }
 
 
 void close_pipes(int pipe_fd[][2]){
-  for(int i = 0; i < cnt_vertline; i++){
+  for(int i = 0; i < pipe_num; i++){
     for(int j = 0; j <= 1 ;j++){
       close(pipe_fd[i][j]);
     }
@@ -494,14 +517,14 @@ int redirect_judgement(char** argk, int cnt) {
 void test_output(vector<string> *redirect_arr, char*** argv_arr, char*** redi_arr) {
   if(redirect_arr->empty() == false) {
     /* print redirect_arr */
-    for(int i = 0; i < cnt_vertline+1; i++){
+    for(int i = 0; i < pipe_num+1; i++){
       cout << "redirect_arr[" << i << "] = " << endl;
       for(vector<string>::iterator it = redirect_arr[i].begin(); it != redirect_arr[i].end(); ++it){
 	cout << *it << endl;
       }
     }
     /* print redi_arr */
-    for(int i = 0; i < cnt_vertline+1; i++){
+    for(int i = 0; i < pipe_num+1; i++){
       cout<< "redi_arr[" << i << "] = " << endl;
       for(int j = 0; redi_arr[i][j] != NULL; j++){
 	cout << redi_arr[i][j] << endl;
@@ -509,7 +532,7 @@ void test_output(vector<string> *redirect_arr, char*** argv_arr, char*** redi_ar
     } 
   }
   /* print argv_arr */
-  for(int i = 0; i < cnt_vertline+1; i++){
+  for(int i = 0; i < pipe_num+1; i++){
     cout<< "argv_arr[" << i << "] = " << endl;
     for(int j = 0; argv_arr[i][j] != NULL; j++){
       cout << argv_arr[i][j] << endl;
